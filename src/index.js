a680871fb33a94562f9f344b0fc2ea26b49ddc4c
Enning94 <enningfestus@gmail.com>
2024-07-17 18:16:27 +0000

    Generate dynamic HTML for product display

--- src/index.js
+++ src/index.js
@@ -1,24 +1,87 @@
-// The main entry point, initializes and connects the other modules.
 import _ from 'lodash';
-// modules/index.js
-// import productService from '../modules/productService.js';
+import '../styles.css';
+import productService from '../modules/productService.js';
 
-// document.addEventListener('DOMContentLoaded', async () => {
-//   try {
-//     const products = await productService.getProducts();
-//     console.log('Products received in index.js:', products);
-//   } catch (error) {
-//     console.error('Error in DOMContentLoaded:', error);
-//   }
-// });
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const products = await productService.getProducts();
+    const productContainer = document.getElementById('card-container')
+    products.forEach(product => {
 
-function component() {
-  const element = document.createElement('div');
+      const productElement = createProductElement(product)
+     
+      productContainer.appendChild(productElement);
+    });
+    } catch (error) {
+    console.error('Error in DOMContentLoaded:', error);
+  }
+});
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+function createProductElement(product){
+  const cardDiv = document.createElement('div');
+  cardDiv.classList.add('card');
 
-  return element;
+  const firstInnerDiv= document.createElement('div');
+
+  const picture = document.createElement('picture');
+
+  const sourceDesktop = document.createElement('source');
+  sourceDesktop.srcset=product.image.desktop;
+  sourceDesktop.media='(min-width:1024px)';
+
+  const sourceTablet = document.createElement('source');
+  sourceTablet.srcset = product.image.tablet;
+  sourceTablet.media='(min-width:768px)';
+
+  const sourceMobile = document.createElement('source');
+  sourceMobile.srcset=product.image.mobile;
+  sourceMobile.media='(max-width:767px)'
+
+
+  const img= document.createElement('img');
+  img.classList.add('thumbnail-img');
+  img.src = product.image.thumbnail;
+  img.alt= product.title
+
+  picture.appendChild(sourceDesktop);
+  picture.appendChild(sourceTablet);
+  picture.appendChild(sourceMobile);
+  // console.log(picture.sourceDesktop);
+
+  const btnDiv=document.createElement('div');
+  const button = document.createElement('button');
+  button.type= 'button';
+  button.classList.add('add-to-cart');
+  button.textContent = 'Add to Cart';
+
+  btnDiv.appendChild(button);
+
+  firstInnerDiv.appendChild(picture);
+  firstInnerDiv.appendChild(btnDiv);
+
+
+const secondInnerDiv = document.createElement('div');
+secondInnerDiv.classList.add('card-body');
+
+const productCategory = document.createElement('h5');
+productCategory.classList.add('card-title');
+productCategory.textContent= product.category;
+
+const productName = document.createElement('p');
+productName.classList.add('card-text');
+productName.textContent=product.name;
+
+const productPrice = document.createElement('p');
+productPrice.classList.add('price-text');
+productPrice.textContent= `$${product.price}`;
+
+secondInnerDiv.appendChild(productCategory);
+secondInnerDiv.appendChild(productName);
+secondInnerDiv.appendChild(productPrice);
+
+cardDiv.appendChild(firstInnerDiv);
+cardDiv.appendChild(secondInnerDiv);
+
+return cardDiv;
+
 }
-
-document.body.appendChild(component());
